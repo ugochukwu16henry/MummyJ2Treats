d882cffd4bcecae7f3a3308d368ec4b8a249e93e
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-02-26 18:24:13 +0100

    update

--- apps/backend/src/modules/orders/orders.module.ts
+++ apps/backend/src/modules/orders/orders.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { DatabaseModule } from '../../database/database.module';
 import { VendorsModule } from '../vendors/vendors.module';
 import { MoatModule } from '../moat/moat.module';
 import { DeliveryModule } from '../delivery/delivery.module';
 
 @Module({
-  imports: [VendorsModule, MoatModule, DeliveryModule],
+  imports: [DatabaseModule, VendorsModule, MoatModule, DeliveryModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
